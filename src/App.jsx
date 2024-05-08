5124a13e4c0c3ec20ad196a2f3ca6b22dc26d061
Joel-Lavia <joeldisidi8@gmail.com>
2024-05-08 15:14:44 +0100

    feat:integration field for UserName and Pictures

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import FieldPictures from './components/FieldPictures'
 
 function App() {
 
   return (
     <>
-
+<FieldPictures/>
     
   </>
   )
