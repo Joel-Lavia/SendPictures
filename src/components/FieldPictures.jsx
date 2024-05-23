1e7d29787b40557a37914be131fc9055d8d8fcf1
Joel-Lavia <joeldisidi8@gmail.com>
2024-05-23 19:53:51 +0100

    feat:send pictures in cloudinary

--- src/components/FieldPictures.jsx
+++ src/components/FieldPictures.jsx
@@ -1,49 +1,70 @@
-import { useState } from "react";
+import {useState } from "react";
 import { useForm } from "react-hook-form"
+import axios from "axios";
 
 function FieldPictures() {
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        formState:{errors}
+    } = useForm();
 
-const [imagePreview, setImagePreview] = useState("");
+    const [imagePreview, setImagePreview] = useState("");
+    // const [url, setUrl] = useState("");
 
-const viewImage  = (e) => {
+    const viewImage = (e) => {
+    const file = e.target.files[0];
 
-const file = e.target.files[0];
-if(file){
-const reader = new FileReader();
-reader.onloadend = () => {
-const imagePath = reader.result; 
-setImagePreview(imagePath);
-console.log(imagePath);
-}
-reader.readAsDataURL(file);
-}
-}
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                const url = reader.result;
+                setImagePreview(url);
+                setValue("pictures",url);
+                console.log(`salut ${url}`);
+            }
 
-const {
-    register,
-    handleSubmit,
-    formState:{errors}
-} = useForm();
- const onSubmit = (data) => console.log(data);
+            reader.readAsDataURL(file);
+        }
+    }
+    
+    const onSubmit = async (data) => {
+
+    // // const author =  data.u
+    // console.log(`salut: ${data}`);
+    console.log(data);
+    // console.log('salut:', JSON.stringify(data, null, 2));
+
+        try {
+            const response  =  await axios.post("http://localhost:4000/PostPictures",data)
+           .then(() => {
+            console.log(response.data);
+           })
+        } 
+        catch (error) {
+        console.log(error); 
+        }
+    }
+    // console.log(`heo${url}`);
 
 return(
 
 <section>
 <form onSubmit={handleSubmit(onSubmit)}>
-
 <label>
 Name
 <p>
-<input {...register('Name',{required:true})} />
-{errors.Name && <p>le nom est requis</p> }
+<input {...register('authorPictures',{required:true})} />
+{errors.authorPictures && <p>le nom est requis</p> }
 </p>  
 </label>   
 
 <label>
 pictures
 <p>
-<input type="file" {...register('Photo',{required:true})} onChange={viewImage} />
-{errors.Photo && <p>file required</p> }
+<input type="file"  onChange={viewImage}/>
+{errors.pictures && <p>file required</p> }
 </p>
 </label>
 
